5796f35a43d1c2aa6db375a98aa95b81140f129e
unknown <alex654v@stud.kea.dk>
2021-04-20 19:46:57 +0200

    nået meget langt

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,43 @@
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintStream;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.*;
 
 public class Main {
-  ArrayList<Integer> orders = new ArrayList<>();
-  private String MARIOSORDERS = "C:/gitrepos/MarioProjekt/src/orders.txt";
+  ArrayList<Order> allOrders = new ArrayList<>();
+  private int numberOfOrders = 0;
+  private String MARIOSORDERS = "orders.txt";
+  private FileOutputStream outPutFile;
   private LocalDateTime localedatetime = LocalDateTime.now();
+  PrintStream outFile;
 
 
   public static void main(String[] args) {
     System.out.println(new File("orders").getAbsolutePath());
-    MenuCard menuCard = new MenuCard();
+
     new Main().run();
+
   }
 
   void run() {
+    loadList1();
+    File logFile = new File(MARIOSORDERS);
+    try {
+      logFile.createNewFile();
+      outPutFile = new FileOutputStream(logFile, true);
+      outFile = new PrintStream(outPutFile);
+
+  } catch (IOException | InputMismatchException e) {
+    System.out.println("IO" + e);
+  }
+
     boolean progess;
     Menu menu = new Menu("Choose option", "choose option", new String[]{
         "1.Display Menu",
         "2.Start Order",
         "3.Display Orders",
-        "4. load order",
+        "4.Delete Order",
         "5.End order"});
 
     progess = true;
@@ -40,8 +52,11 @@ public class Main {
         case 2:
           enterOrder();
           break;
+        case 3:
+          displayOrder();
+          break;
         case 4:
-          loadList1();
+          deleteOrder();
           break;
         case 5:
           progess = false;
@@ -52,57 +67,86 @@ public class Main {
     }
   }
 
-    void displayMenu() {
-      System.out.println("\nDisplay Menu");
-      for (int i = 0; i < orders.size(); i++) {
-        System.out.println("#" + i + ": " + orders.get(i));
-      }
-    }
 
-  public ArrayList<Integer> getOrders() {
-    return orders;
+  public ArrayList<Order> getOrders() {
+    return allOrders;
   }
 
-  public void setOrders(ArrayList<Integer> orders) {
-    this.orders = orders;
+  public void setOrders(ArrayList<Order> orders) {
+    this.allOrders = orders;
   }
 
-  void enterOrder(){
+  void enterOrder() {
     Scanner scan = new Scanner(System.in);
-      System.out.println("Enter the number of the pizza you wanna order TEST");
-      int number = scan.nextInt();
-      while (number != 0)
-      {
-        orders.add(number);
-        number = scan.nextInt();
-      }
-      System.out.println(orders);
-      try {
-        PrintStream outFile = new PrintStream(new File(MARIOSORDERS));
-        for (int i = 0; i < orders.size(); i++) {
-          outFile.println(orders.get(i));
-
-        }
-        outFile.println("\n" + localedatetime);
-      }
-      catch (InputMismatchException | FileNotFoundException e) {
-        System.out.println("IO" + e);
-      }
+    System.out.println("Enter the number of the pizza you wanna order?");
+    int number = scan.nextInt();
+    ArrayList<Integer> orders = new ArrayList<>();
+    while (number != 0) {
+      orders.add(number);
+      number = scan.nextInt();
     }
+    numberOfOrders++;
+    Order order = new Order(numberOfOrders, orders, LocalDateTime.now().toString());
+    System.out.println(order);
+    allOrders.add(order);
+    outFile.println(order);
 
-  void loadList1(){
-    ArrayList<Integer> ordersFromFile = new ArrayList<>();
+  }
+
+  void loadList1() {
+
     try {
       Scanner filereader = new Scanner(new File(MARIOSORDERS));
-      while(filereader.hasNext()){
+      while (filereader.hasNext()) {
         String f = filereader.nextLine();
-        ordersFromFile.add(Integer.valueOf(f));
+        String[] split = f.split(",");
+        numberOfOrders = Integer.parseInt(split[0]);
+        String time = split[1];
+        ArrayList<Integer> pizza = new ArrayList<>();
+        for (int i = 2; i < split.length; i++){
+          pizza.add(Integer.parseInt(split[i]));
+        }
+          Order order = new Order(numberOfOrders, pizza ,time );
+
+        allOrders.add(order);
       }
-      orders = ordersFromFile;
       System.out.println("loaded");
-    } catch (InputMismatchException | FileNotFoundException e){
+    } catch (InputMismatchException | FileNotFoundException e) {
       System.out.println("IO" + e);
     }
   }
+    void displayOrder () {
+    for (int i = 0; i < allOrders.size(); i++){
+      System.out.println(allOrders.get(i));
+
+    }
+    }
+    void deleteOrder (){
+    Scanner scan = new Scanner(System.in);
+      System.out.println("Enter order number you want to delete");
+    int orderNr = scan.nextInt();
+      for (int i = 0; i < allOrders.size(); i++){
+        if (orderNr == allOrders.get(i).orderNr){
+          allOrders.remove(allOrders.get(i));
+          resetLogFile();
+          break;
+        }
+      }
+
+    }
+    void resetLogFile(){
+      try {
+        outPutFile = new FileOutputStream(MARIOSORDERS, false);
+        outFile = new PrintStream(outPutFile);
+
+      } catch (IOException | InputMismatchException e) {
+        System.out.println("IO" + e);
+      }
+      for (int i = 0; i < allOrders.size(); i++){
+        outFile.println(allOrders.get(i));
+
+      }
+    }
   }
+
 
--- src/Order.java
+++ src/Order.java
@@ -1,16 +1,35 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Order {
-  String id;
-  String time;
+  int orderNr;
+  ArrayList<Integer> pizzas;
+  String localdatetime;
 
-  public void displayMenu(){
-
+  public Order(int orderNr, ArrayList<Integer> pizzas, String now) {
+    this.orderNr = orderNr;
+    this.pizzas = pizzas;
+    this.localdatetime = now;
   }
+  public String toString(){
+    StringBuilder s = new StringBuilder();
+    s.append(orderNr);
+    s.append(",");
+    s.append(localdatetime);
+    for (int i = 0; i < pizzas.size(); i++){
+      s.append(",");
+      s.append(pizzas.get(i));
+    }
+    // 1, 1 jan, 2, 3
+    return s.toString();
+  }
+
+
   public void createOrder(){
 
   }
-  public void displayOrder(){
 
   }
-}
+
 
 
